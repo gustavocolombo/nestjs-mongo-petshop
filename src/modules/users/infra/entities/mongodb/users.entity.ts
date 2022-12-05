e9fbb746e37ccef8cf6e3a1aa9d1fa971bb7be7d
Gustavo Colombo Rocha <gusttavocolombo@gmail.com>
2022-12-05 19:04:14 -0300

    feat: setup docs from Swagger to API

--- src/modules/users/infra/entities/mongodb/users.entity.ts
+++ src/modules/users/infra/entities/mongodb/users.entity.ts
@@ -1,10 +1,12 @@
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
-import { HydratedDocument } from 'mongoose';
+import mongoose, { HydratedDocument } from 'mongoose';
 
 export type UserDocument = HydratedDocument<User>;
 
 @Schema()
 export class User {
+  _id: mongoose.Schema.Types.ObjectId;
+
   @Prop({ required: true })
   firstname: string;
 
